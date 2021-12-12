733d0fd1466c022bdd240586551dd88f6fc7cc38
Wellington Oliveira dos Santos <welblade@gmail.com>
2021-12-11 21:50:25 -0300

    * Adicionada resposta de valores das moedas vindas da da api

--- app/src/main/java/br/com/dio/coinconverter/data/service/AwesomeService.kt
+++ app/src/main/java/br/com/dio/coinconverter/data/service/AwesomeService.kt
@@ -1,4 +1,10 @@
 package br.com.dio.coinconverter.data.service
 
+import br.com.dio.coinconverter.data.model.ExchangeResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface AwesomeService {
+    @GET("/last/{coins}")
+    suspend fun exchangeValue(@Path("coins") coins: String):ExchangeResponse
 }
